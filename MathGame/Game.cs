5452e822a69032644da84f522c0b2f7c6cb2af1b
luisdevvi <luis.devvi@gmail.com>
2024-12-01 21:18:16 -0300

    add Sum game function

---
+++ MathGame/Game.cs
@@ -0,0 +1,50 @@
+﻿namespace MathGame
+{
+    internal class Game
+    {
+        Random random = new Random();
+
+        int numberOne = 0;
+        int numberTwo = 0;
+        int result = 0;
+        int userAnswer = 0;
+        int userScore = 0;
+
+        public void Sum()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                numberOne = random.Next(1, 11);
+                numberTwo = random.Next(1, 11);
+                result = numberOne + numberTwo;
+
+                Console.WriteLine($"What's the result for the Sum of {numberOne} + {numberTwo}");
+                userAnswer = int.Parse(Console.ReadLine());
+
+                if (userAnswer == result)
+                {
+                    Console.WriteLine("Yay, you got it right! You score +1 point!");
+                    userScore += 1;
+                    Console.WriteLine($"Current score: {userScore}\n");
+                    Thread.Sleep(1000);
+                }
+                else
+                {
+                    Console.WriteLine("Oh no, you got it wrong!");
+                    Console.WriteLine($"Current score: {userScore}\n");
+                    Thread.Sleep(1000);
+                }
+            }
+
+            Console.WriteLine($"Game result: {userScore} points");
+
+            //Console.WriteLine("You want to play another game? Y/N");
+            //string anotherGameOption = Console.ReadLine().ToLower();
+
+            //if (anotherGameOption == "y")
+            //{
+
+            //}
+        }
+    }
+}
